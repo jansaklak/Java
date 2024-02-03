cc25442b943efaae24b012c2d515859da5bf65a6
jansaklak <101001672+jansaklak@users.noreply.github.com>
2024-02-03 16:19:29 +0100

    Update XML.java

--- XML/XML.java
+++ XML/XML.java
@@ -6,4 +6,3 @@ XMLDecoder:
     XMLDecoder d = new XMLDecoder(new FileInputStream("jbutton.xml"));
         obj = d.readObject();
         d.close(); 
-//nie mieszać html z kodem bo graficy od html muszą zmieniać kod aplikacji. wygląd niezależny i programista tylko wypełnia  
